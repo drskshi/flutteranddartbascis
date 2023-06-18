300ff767a7b0596cf2d116f22632c231922aacc8
Chitiz <111699407+drskshi@users.noreply.github.com>
2023-06-17 23:00:22 -0700

    Update Loops.dart

--- Loop/Loops.dart
+++ Loop/Loops.dart
@@ -1,9 +1,9 @@
 void main(List<String> args) {
-  // for (int i = 0; i <= 10; i++) {
-  //   if (i % 2 == 0) {
-  //     print(i);
-  //   }
-  // }
+  for (int i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
   List cities = ["London", "Kathmandu", "Bharatpur"];
   for (String city in cities) {
     print("$city");
