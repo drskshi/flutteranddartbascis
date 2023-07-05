f77442e78560f77b34be8c07397a7decda27e871
drskshi <duwadiksh15@gmail.com>
2023-07-05 03:13:50 -0700

    Loop

--- Loop/Loops.dart
+++ Loop/Loops.dart
@@ -1,11 +1,25 @@
-void main(List<String> args) {
-  // for (int i = 0; i <= 10; i++) {
-  //   if (i % 2 == 0) {
-  //     print(i);
-  //   }
-  // }
+Future<void> main(List<String> args) async {
+  //For loops
+
+  for (int i = 0; i <= 10; i++) {
+    if (i % 2 == 0) {
+      print(i);
+    }
+  }
   List cities = ["London", "Kathmandu", "Bharatpur"];
   for (String city in cities) {
     print("$city");
   }
+  int A = 1;
+  while (A < 5) {
+    print(A);
+    ++A;
+  }
+
+  print("-----------------");
+  int b = 1;
+  do {
+    print(b);
+    b++;
+  } while (b <= 8);
 }
---
+++ Loop/Whileanddowhileloops.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  //while loop
+  int a = 1;
+  while (a < 5) {
+    print(a);
+    a++;
+  }
+  print("#####");
+  //dowhileloop
+  int b = 0;
+  do {
+    print(b);
+    b++;
+  } while (b <= 5);
+}
